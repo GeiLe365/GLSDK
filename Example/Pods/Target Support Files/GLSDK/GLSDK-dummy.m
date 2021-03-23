324446fdbc1f5863791e72e7052e85a5da179db9
dfs930 <dfs930@365gl.com>
2021-03-23 18:07:07 +0800

    commit

---
+++ Example/Pods/Target Support Files/GLSDK/GLSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GLSDK : NSObject
+@end
+@implementation PodsDummy_GLSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-GLSDK_Example/Pods-GLSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GLSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GLSDK_Example
+@end
